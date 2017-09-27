202a44151a50386733f81caebf5c0d8d50c39d5e
Nathan Lanza <lanza@fb.com>
2017-09-27 16:46:55 -0700

    asdf

--- Tests/ReuseTests.swift
+++ Tests/ReuseTests.swift
@@ -13,16 +13,10 @@ class ReuseTests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func test_tripleQuestionMark_BehavesProperly() {
+        let q: Int? = nil
+        
+        XCTAssert("hi \(q ??? "no value")" == "hi no value")
     }
     
 }
